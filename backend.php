af6de377c3011b6bdb5350503e2a63da958cdb2a
seocodes <azanettev@gmail.com>
2025-08-19 14:27:49 -0300

    ajuste

--- backend.php
+++ backend.php
@@ -50,7 +50,6 @@
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
         $speciesResponse = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
         curl_close($ch);
 
